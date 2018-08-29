403c6b4fe82bf6e5effa835fa78d0a587fe01ab8
Marianne Rachid <marianne.rachid@gmail.com>
2018-08-29 12:21:26 +0100

    Returns a string with a square shape that is as long and wide as the integer when given number 10

--- spec/build_a_square_spec.rb
+++ spec/build_a_square_spec.rb
@@ -4,4 +4,8 @@ describe 'generate_shape' do
   it 'returns a string with a square shape when given 3' do
     expect(generate_shape(3)).to eq "+++\n+++\n+++"
   end
+
+  it 'returns a string with a square shape when given 10' do
+    expect(generate_shape(10)).to eq "++++++++++\n++++++++++\n++++++++++\n++++++++++\n++++++++++\n++++++++++\n++++++++++\n++++++++++\n++++++++++\n++++++++++"
+  end
 end
